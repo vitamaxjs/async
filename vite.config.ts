5e9483f64329bbfc3a2e02b1309bb12a10bdfe5f
rickygao <rckgao@gmail.com>
2025-11-08 16:12:46 +0800

    Build by vite

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from "node:path";
+import dts from "unplugin-dts/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "vAsync",
+    },
+  },
+  plugins: [
+    dts({
+      tsconfigPath: path.resolve(__dirname, "tsconfig.lib.json"),
+      bundleTypes: true,
+    }),
+  ],
+});
