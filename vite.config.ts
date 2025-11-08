5d981fc53b5638487e5606fe3c5422a19d8226a3
rickygao <rckgao@gmail.com>
2025-11-08 16:39:30 +0800

    Change global exposed name to vitamax.async

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "vAsync",
+      name: "vitamax.async",
     },
   },
   plugins: [
